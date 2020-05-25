67bd6d56b0cdfaf4e259f8d414d47ea743408822
Dom-888 <spinelladomenico88@gmail.com>
2020-05-25 22:06:19 +0000

    Move delete-confirmation modal opener from HTML to JS

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,8 @@
 // Replace all elements that have a data-feather attribute with a Feather icon
 feather.replace()
 
-
-
-// Take the trailer id from btn-trash-can and use it to fill the href attribute of btn-delete
-function passTrailerId(trailer) {
+function loadDeleteModal(trailer) {
+    $('#modal-delete-confirmation').modal("show")
     let trailerId = trailer.getAttribute("data-trailer-id");
     let complete_url = "/delete_trailer/" + trailerId
     $('#btn-delete').attr("href", complete_url)
@@ -12,11 +10,9 @@ function passTrailerId(trailer) {
 
 function loadEditModal(trailer) { 
     $('#modal-edit-trailer').modal("show");
-
     let trailerId = trailer.getAttribute("data-trailer-id");
     let complete_url = "/update_trailer/" + trailerId
     $('#btn-update').attr("href", complete_url)
-
     let trailerTitle = trailer.getAttribute("data-trailer-title");
     let trailerUrl = trailer.getAttribute("data-trailer-url");
     let trailerQuote = trailer.getAttribute("data-trailer-quote");
