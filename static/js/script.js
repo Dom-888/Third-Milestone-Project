db4c377f95887255410bc7cf08b9012e70dbc778
Dom-888 <spinelladomenico88@gmail.com>
2020-05-25 21:55:39 +0000

    Add edit-trailer modal

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,26 @@
 // Replace all elements that have a data-feather attribute with a Feather icon
 feather.replace()
 
+
+
 // Take the trailer id from btn-trash-can and use it to fill the href attribute of btn-delete
 function passTrailerId(trailer) {
     let trailerId = trailer.getAttribute("data-trailer-id");
     let complete_url = "/delete_trailer/" + trailerId
     $('#btn-delete').attr("href", complete_url)
+}
+
+function loadEditModal(trailer) { 
+    $('#modal-edit-trailer').modal("show");
+
+    let trailerId = trailer.getAttribute("data-trailer-id");
+    let complete_url = "/update_trailer/" + trailerId
+    $('#btn-update').attr("href", complete_url)
+
+    let trailerTitle = trailer.getAttribute("data-trailer-title");
+    let trailerUrl = trailer.getAttribute("data-trailer-url");
+    let trailerQuote = trailer.getAttribute("data-trailer-quote");
+    $('#title').attr("value", trailerTitle)
+    $('#url').attr("value", trailerUrl)
+    $('#quote').attr("value", trailerQuote)
 }
