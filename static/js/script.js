4c3f46694177ef77ca2f2b655aba33ee08defc30
Dom-888 <spinelladomenico88@gmail.com>
2020-05-26 11:09:59 +0000

    Merge add-trailer modal with edit-trailer modal

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,14 @@
 // Replace all elements that have a data-feather attribute with a Feather icon
 feather.replace()
 
+function loadAddModal(trailer) {
+    $('#modal-add-edit-trailer').modal("show");
+    let complete_url = "/insert_trailer";
+    $('#modal-add-edit-trailer form').attr("action", complete_url);
+    $('#modal-add-edit-trailer .modal-title').text("Add a new Trailer")
+    $('#modal-add-edit-trailer input').attr("value", "")
+}
+
 function loadDeleteModal(trailer) {
     $('#modal-delete-confirmation').modal("show")
     let trailerId = trailer.getAttribute("data-trailer-id");
@@ -9,14 +17,14 @@ function loadDeleteModal(trailer) {
 }
 
 function loadEditModal(trailer) { 
-    $('#modal-edit-trailer').modal("show");
+    $('#modal-add-edit-trailer').modal("show");
     let trailerId = trailer.getAttribute("data-trailer-id");
     let complete_url = "/update_trailer/" + trailerId
-    // let complete_url = "{{ url_for('update_trailer', trailer_id=trailerId) }}"
-    $('#modal-edit-trailer form').attr("action", complete_url)
+    $('#modal-add-edit-trailer form').attr("action", complete_url);
     let trailerTitle = trailer.getAttribute("data-trailer-title");
     let trailerUrl = trailer.getAttribute("data-trailer-url");
     let trailerQuote = trailer.getAttribute("data-trailer-quote");
+    $('#modal-add-edit-trailer .modal-title').text("Modify "+ trailerTitle)
     $('#title').attr("value", trailerTitle)
     $('#url').attr("value", trailerUrl)
     $('#quote').attr("value", trailerQuote)
