c196afa6601ba4c976ef9338e173a770e63f5c9e
Dom-888 <spinelladomenico88@gmail.com>
2020-06-08 10:52:53 +0000

    Move background image in a dedicated class

--- app.py
+++ app.py
@@ -74,6 +74,12 @@ def update_trailer(trailer_id):
     })
     return redirect(url_for('get_trailers'))
 
+# 404 error page
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 
 if __name__ == '__main__':  
     app.run(host=os.getenv("IP", "0.0.0.0"),
