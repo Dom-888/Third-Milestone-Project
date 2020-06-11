6ebb6f1274f709e06359ad53608fe126e6f8b993
Dom-888 <spinelladomenico88@gmail.com>
2020-06-11 22:06:05 +0000

    Remove debug=true from app.py

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def page_not_found(e):
 
 if __name__ == '__main__':  
     app.run(host=os.getenv("IP", "0.0.0.0"),
-    port=int(os.getenv("PORT", "5000")), debug=True) # Remove dubug=True before publishing
+    port=int(os.getenv("PORT", "5000")), debug=False)
